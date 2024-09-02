2c21576d3389aa9a46009b047260c79f9b5d6558
Daver <dennis.fleer@outlook.de>
2024-09-02 18:34:51 +0200

    Started implementing teleport command

--- BuildersUniverse/src/main/java/de/daver/build/universe/command/world/WorldTeleportSubCommand.java
+++ BuildersUniverse/src/main/java/de/daver/build/universe/command/world/WorldTeleportSubCommand.java
@@ -1,7 +1,11 @@
 package de.daver.build.universe.command.world;
 
-import de.daver.build.hub.api.command.Command;
-import de.daver.build.hub.api.command.CommandBuilder;
+import de.daver.build.hub.UniverseHub;
+import de.daver.build.hub.api.command.*;
+import de.daver.build.hub.api.gate.WorldSlave;
+import de.daver.build.hub.api.util.Sender;
+import de.daver.build.hub.api.util.User;
+import de.daver.build.hub.api.world.World;
 import de.daver.build.universe.util.Permissions;
 
 public class WorldTeleportSubCommand {
@@ -9,8 +13,19 @@ public class WorldTeleportSubCommand {
     public static Command create() {
         return CommandBuilder.create("teleport")
                 .alias("tp")
+                .arguments(ArgumentBuilder.create("id", 0)
+                        .action(WorldTeleportSubCommand::teleportAction)
+                        .build())
                 .permission(Permissions.COMMAND_WORLD_TELEPORT)
                 .build();
+    }
+
+    private static boolean teleportAction(Sender sender, CommandInput input) {
+        String id = input.get("id");
+        WorldSlave slave = UniverseHub.gate().getWorldSlave();
+        World world = UniverseHub.getWorldMaster().getWorld(id);
+        slave.sendTo((User) sender, world);
+        return true;
     }
 
 }
